fb601d14371076ee893c049c2ae24ceb27c323d3
GuntasJ <guntasjammu@gmail.com>
2023-09-09 11:04:04 -0500

    Initial commit

---
+++ src/main/java/com/tags/cs451r_groupproject_backend/Cs451RGroupProjectBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.tags.cs451r_groupproject_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs451RGroupProjectBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs451RGroupProjectBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tags/cs451r_groupproject_backend/Cs451RGroupProjectBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tags.cs451r_groupproject_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs451RGroupProjectBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
